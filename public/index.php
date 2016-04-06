f976deeb5391fd6b7672eb1e20fee2db21ea7086
raby <raby@rabydeMacBook-Air.local>
2016-04-06 23:04:02 +0700

    a

--- public/index.php
+++ public/index.php
@@ -21,9 +21,9 @@ EOT;
 
 $a = array();
 $volumeInfo['volumeInfo']['title'] = "raby";
-$volumeInfo['volumeInfo']['authors'] = "raby";
-$volumeInfo['volumeInfo']['description'] = "raby";
-$volumeInfo['volumeInfo']['imageLinks'] = "raby";
+$volumeInfo['volumeInfo']['authors'] = "authors";
+$volumeInfo['volumeInfo']['description'] = "Leonid McGill can't say no to the beautiful woman";
+$volumeInfo['volumeInfo']['imageLinks']['thumbnail'] = "http://books.google.co.th/books/content?id=mKBOehKRx1kC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api";
 
 array_push($a,$volumeInfo);
 echo json_encode($a);
