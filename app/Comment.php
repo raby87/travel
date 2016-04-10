5788c73cc19c011981a15c279fa98beffedb44c2
raby <raby@rabydeMacBook-Air.local>
2016-04-10 20:47:21 +0700

    a

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $primaryKey = 'cid';
+
+    public function user()
+    {
+        return $this->belongsTo('App\User','to','uid');
+    }
+    //追加的评论
+    public function userAttach()
+    {
+        return $this->belongsTo('App\User','to','uid');
+    }
 }
--- app/Http/Controllers/Rest/TravelController.php
+++ app/Http/Controllers/Rest/TravelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Rest;
 
+use App\Comment;
 use App\User;
 use App\Travel;
 use Validator;
@@ -12,25 +13,68 @@ class TravelController extends Controller
 
     public function index()
     {
-        $travel = Travel::with('user')->with('image')->orderBy('init_time', 'desc')->take(10)->get();
+        $travels = Travel::with('user')->with('image')->orderBy('init_time', 'desc')->take(10)->get();
 
-dd($travel);
+        $result = [];
+        foreach($travels as $k=>$travel){
+            $tmp = [
+                'tid'=>$travel->tid,
+                'uid'=>$travel->uid,
+                'icon'=>$travel->user->icon,
+                'name'=>$travel->user->name,
+                'sex'=>$travel->user->sex,
+                'age'=>$travel->user->age,
 
-        return response()->json([
-            'tid'=>$travel->tid,
-            'uid'=>$travel->uid,
-            'icon'=>$travel->user->icon,
-            'name'=>$travel->user->name,
-            'sex'=>$travel->user->sex,
-            'age'=>$travel->user->age,
+                'init_time'=>$travel->init_time,
+                'content'=>$travel->content,
+                'comment_stat'=>$travel->comment_stat,
+                'join_stat'=>$travel->join_stat,
+                'like_stat'=>$travel->like_stat,
 
-            'init_time'=>$travel->init_time,
-            'content'=>$travel->content,
-            'comment_stat'=>$travel->comment_stat,
-            'join_stat'=>$travel->join_stat,
-            'like_stat'=>$travel->like_stat,
+                'image_list'=>$travel->image->toJson(),
+            ];
+            array_push($result,$tmp);
+        }
+        return response()->json($result);
+    }
 
-            'image_list'=>$travel->image->toJson(),
-        ]);
+    /**
+     * todo 待优化，待增加追加的评论
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function detail(){
+        $tid = 1;
+        $result = [];
+        $travel = Travel::find($tid)->with('user')->with('image')->orderBy('init_time', 'desc')->first();
+        //$result['travel'] = $travel->toJson();
+        //$result['travel'][] = $travel->toJson();
+        $result['travel'] = [
+                'tid'=>$travel->tid,
+                'uid'=>$travel->uid,
+                'icon'=>$travel->user->icon,
+                'name'=>$travel->user->name,
+                'sex'=>$travel->user->sex,
+                'age'=>$travel->user->age,
+
+                'init_time'=>$travel->init_time,
+                'content'=>$travel->content,
+                'comment_stat'=>$travel->comment_stat,
+                'join_stat'=>$travel->join_stat,
+                'like_stat'=>$travel->like_stat,
+                'image_list'=>$travel->image->toJson(),
+            ];
+
+        $comments = Comment::find($tid)->with('user')->orderBy('init_time', 'desc')->take(10)->get();
+
+        $comment_tmp = [];
+        foreach ($comments as $k=>$comment) {
+            $tmp = $comment;
+            $tmp['user'] = $comment->user->toJson();
+            array_push($comment_tmp,$tmp);
+        }
+
+        $result['comment'] = $comment_tmp;
+
+        return response()->json($result);
     }
 }
