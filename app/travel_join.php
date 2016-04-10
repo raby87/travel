203e6cd7ba85315f2fc20d139c4051e628d39b22
root <root@CentOS6.3>
2016-04-10 16:40:09 +0800

    a

---
+++ app/travel_join.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class travel_join extends Model
+{
+    //
+}
---
+++ pull.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /var/www/public_html/run/
+git pull
