81e7406e888c9c367c9908354c8f3da6c2219a28
raby <raby@rabydeMacBook-Air.local>
2016-04-10 12:02:58 +0700

    a

--- public/mysql.php
+++ public/mysql.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('127.0.0.1', 'root', 'mysql', 'test')
+$link = mysqli_connect('localhost', 'root', 'mysql', 'test')
 or die('Could not connect: ' . mysqli_error());
 echo 'Connected successfully';
 
