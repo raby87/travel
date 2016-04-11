11ef1e7c470faeff8e62e3c54a592005abfac99f
raby <raby@rabydeMacBook-Air.local>
2016-04-11 20:43:19 +0700

    a

--- app/Http/Controllers/Rest/TravelController.php
+++ app/Http/Controllers/Rest/TravelController.php
@@ -72,7 +72,9 @@ class TravelController extends Controller
         $rs = Storage::disk('local')->put('test.txt', 'Contents');
 
 
-        Storage::copy('/var/www/public_html/7kanya/www/Home/Public/img/clinic/50118/2_1379831629.9642.jpg','1.jpg');
+        //Storage::copy('/var/www/public_html/7kanya/www/Home/Public/img/clinic/50118/2_1379831629.9642.jpg','1.jpg');
+        $file = "/var/www/public_html/7kanya/www/Home/Public/img/clinic/50118/2_1379831629.9642.jpg";
+        Storage::disk('local')->put('1.jpg', file_get_contents($file));
         return response()->json($rs);
     }
 }
