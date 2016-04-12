a28a00af471eb1404f4267d0ddb3639ebdf94410
raby <raby@rabydeMacBook-Air.local>
2016-04-13 01:05:09 +0700

    a

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,5 +7,22 @@
 @stop
 
 @section("content")
-    login
+    <h1 class="page-header">登 录</h1>
+    <form action="" method="post" class="form-horizontal">
+        <div class="form-group">
+            <div class="col-sm-5">
+                <input type="text" class="form-control"     placeholder="请输入用户名"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-5">
+                <input type="password" class="form-control" placeholder="请输入密码/email"/>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-5">
+                <button class="btn-primary">登 录</button>
+            </div>
+        </div>
+    </form>
 @stop
