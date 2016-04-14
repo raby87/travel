b6dd25330cffc096b2f67c92df713c4f167977ed
raby <raby@rabydeMacBook-Air.local>
2016-04-14 10:17:48 +0700

    a

---
+++ tests/FooTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FooTest extends TestCase {
+
+    public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
