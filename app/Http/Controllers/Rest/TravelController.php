2925f0e09dc5938c069728f963ff981ff29cb681
raby <raby@rabydeMacBook-Air.local>
2016-04-14 11:43:43 +0700

    a

--- app/Http/Controllers/Rest/TravelController.php
+++ app/Http/Controllers/Rest/TravelController.php
@@ -76,7 +76,7 @@ class TravelController extends Controller
         $travel = new Travel();
         $travel->uid = 1;
         $travel->content = "mymy";
-        $travel->save();
+        $rs = $travel->save();
 
         //$file = "/var/www/public_html/7kanya/www/Home/Public/img/clinic/50118/2_1379831629.9642.jpg";
         //Storage::disk('local')->put('1.jpg', file_get_contents($file));
