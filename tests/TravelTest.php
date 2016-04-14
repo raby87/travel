d4ea6f34a2231a488be031c146e086984b0caa29
raby <raby@rabydeMacBook-Air.local>
2016-04-14 11:45:29 +0700

    a

--- tests/TravelTest.php
+++ tests/TravelTest.php
@@ -9,8 +9,7 @@ class TravelTest extends TestCase {
     public function testPublish()
     {
         $response = $this->call("POST", "rest/publish", ['uid'=>1,'content'=>'test contnet']);
-        $this->assertEquals('Hello World', $response->getContent());
-        //$this->assertTrue(true);
+        $this->assertTrue($response->getContent());
     }
 
 }
