2895df0fd7e3e622dbd4c5d9346e8e5a023734a3
raby <raby@rabydeMacBook-Air.local>
2016-04-19 20:38:42 +0700

    a

--- resources/views/admin/authLogin.blade.php
+++ resources/views/admin/authLogin.blade.php
@@ -9,9 +9,7 @@
 @section("content")
     <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=1817611297&debug=true" type="text/javascript" charset="utf-8"></script>
     <h1 class="page-header">登 录</h1>
-    <wb:login-button type="3,2" ></wb:login-button>
-
-    <div id="wb_connect_btn1" onclick="login()"></div>
+    <div id="wb_connect_btn1" onclick="login()">登录</div>
     <script type="application/javascript" >
         function login(){
             WB2.login(function() {
