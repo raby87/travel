34e987e0339ccc91f6c682f755bc40b6eb513888
raby <raby@rabydeMacBook-Air.local>
2016-04-19 19:58:53 +0700

    a

--- resources/views/admin/authLogin.blade.php
+++ resources/views/admin/authLogin.blade.php
@@ -9,21 +9,21 @@
 @section("content")
     <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=1817611297&debug=true" type="text/javascript" charset="utf-8"></script>
     <h1 class="page-header">登 录</h1>
-    <div id="wb_connect_btn"></div>
+    <div id="wb_connect_btn1"></div>
     <script type="application/javascript" >
         WB2.anyWhere(function (W) {
-        W.widget.connectButton({
-        id: "wb_connect_btn",
-        type: '3,2',
-        callback: {
-        login: function (o) { //登录后的回调函数
-        alert("login: " + o.screen_name)
-        },
-        logout: function () { //退出后的回调函数
-        alert('logout');
-        }
-        }
-        });
+            W.widget.connectButton({
+                id: "wb_connect_btn1",
+                type: '3,2',
+                callback: {
+                login: function (o) { //登录后的回调函数
+                alert("login: " + o.screen_name)
+                },
+                logout: function () { //退出后的回调函数
+                alert('logout');
+                }
+                }
+            });
         });
     </script>
 @stop
