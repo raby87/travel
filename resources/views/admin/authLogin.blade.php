6953edd75d20d666170cc2bea9ae205f7023ee4c
raby <raby@rabydeMacBook-Air.local>
2016-04-20 20:02:18 +0700

    a

--- resources/views/admin/authLogin.blade.php
+++ resources/views/admin/authLogin.blade.php
@@ -9,12 +9,12 @@
 @section("content")
     <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=1817611297&debug=true" type="text/javascript" charset="utf-8"></script>
     <h1 class="page-header">登 录</h1>
-    <div id="wb_connect_btn1" onclick="login()">登录</div>
+    <div class="btn btn-default" onclick="login()">登录</div>
     <script type="application/javascript" >
         //https://api.weibo.com/oauth2/authorize?client_id=1817611297&
         // redirect_uri=http://run.51094.com&response_type=code
         function login(){
-           window.location.href="https://api.weibo.com/oauth2/authorize?client_id=1817611297&redirect_uri=http://run.51094.com&response_type=code";
+           window.location.href="https://api.weibo.com/oauth2/authorize?client_id=1817611297&redirect_uri=http://run.51094.com/info&response_type=code";
         }
     </script>
 @stop
