850563ca2fa5544f9b93988e0e4edd02e314c9fc
raby <raby@rabydeMacBook-Air.local>
2016-04-20 20:23:10 +0700

    a

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,8 +42,8 @@ Route::get('/', function () {
     $server_output = curl_exec ($ch);
 
     curl_close ($ch);
-    return "".$server_output;
-    //return view('welcome');
+    echo "".$server_output;
+    return view('welcome');
 });
 
 
