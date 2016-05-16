921ecd6def9d54dedb4ca677706c596ceeec3ce3
raby <raby@rabydeMacBook-Air.local>
2016-05-16 23:17:54 +0700

    a

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -9,8 +9,61 @@ use Validator;
 use App\Http\Controllers\Controller;
 
 class Table{
+    private $source = [];
+    private $titles = [];
 
-    public function make($source,$title){
+    private $org = [];
+
+    public function __construct($org=[]){
+        $this->org = $org;
+    }
+    public function addLeft($title="",$col=[]){
+        array_unshift($this->titles,$title);
+        array_unshift($this->source,$col);
+        return $this;
+    }
+    public function addRight($title="",$col=[]){
+        array_unshift($this->titles,$title);
+        array_unshift($this->source,$col);
+        return $this;
+    }
+    public function addCol($titles=[]){
+        $keys = array_keys($titles);
+        $values = array_values($titles);
+        foreach($keys as $k=>$v){
+           $row = array_column($this->org,$v);
+            array_push($this->source,$row);
+        }
+        $this->titles = $values;
+        return $this;
+    }
+    /**
+     * @param string $col
+     * @param string $url   http://www.baidu.com/index.php?do=testController&aid=%s
+     */
+    public function addDecorator($col="",$url=[]){
+        $cols = array_column($this->source,$col);
+        
+    }
+
+    /**
+     * todo 一次性渲染
+     * @param $source
+     */
+    public function make(){
+
+    }
+
+    /**
+     * todo 用于js绑定方法
+     */
+    public function addClass(){
+
+    }
+    /**
+     * todo 用于js绑定方法
+     */
+    public function addAttr(){
 
     }
 }
