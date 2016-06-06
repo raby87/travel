547b0469c77900b34077a1ed79c02c4a30842489
raby <raby@rabydeMacBook-Air.local>
2016-06-06 21:07:28 +0700

    a

--- resources/views/jdk/login.blade.php
+++ resources/views/jdk/login.blade.php
@@ -7,7 +7,15 @@
 @stop
 
 @section("content")
-    <span id="qqLoginBtn" class="btn btn-success">QQ登录</span>
+    <div class="panel">
+        <div class="panel-heading">第三方接口</div>
+        <div class="panel-body">
+            <span id="qqLoginBtn" class="btn btn-success">QQ登录</span>
+
+            <span id="weiboLoginBtn" class="btn btn-success">weibo登录</span>
+        </div>
+    </div>
+
     <script type="text/javascript">
         $( "#qqLoginBtn" ).click(function() {
             var url = "https://graph.qq.com/oauth2.0/authorize?response_type=code&" +
@@ -15,10 +23,7 @@
 
             window.location.href=url;
         });
-    </script>
 
-    <span id="weiboLoginBtn" class="btn btn-success">weibo登录</span>
-    <script type="text/javascript">
         $( "#weiboLoginBtn" ).click(function() {
             var url = "https://api.weibo.com/oauth2/authorize?client_id=3453585210&" +
                     "redirect_uri=http://www.kcdlife.com?type=weibo&response_type=code";
